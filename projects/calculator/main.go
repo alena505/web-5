3ca5fbc19c7946dd4e4ea87dd36ae859d54a6681
Алена Авдеева <alenaavdeeva@MacBook-Pro-Alena.local>
2024-10-07 23:41:30 +0300

    main.go1

--- projects/calculator/main.go
+++ projects/calculator/main.go
@@ -1,7 +1,31 @@
 package main
 
-// реализовать calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+import "fmt"
+
+func calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int {
+	a := make(chan int)
+	go func(b chan int) {
+		defer close(b)
+		select {
+		case c := <-firstChan:
+			b <- c * c
+		case d := <-secondChan:
+			b <- d * 3
+		case <-stopChan:
+			return
+		}
+
+	}(a)
+
+	return a
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+	ch1, ch2 := make(chan int), make(chan int)
+	stop := make(chan struct{})
+	r := calculator(ch1, ch2, stop)
+	//ch1 <- 3
+	//ch2 <- 4
+	close(stop)
+	fmt.Println(<-r)
 }
